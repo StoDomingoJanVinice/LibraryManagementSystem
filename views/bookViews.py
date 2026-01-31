dc70edecd36e5c9a003b6911a19c14c61e715999
StoDomingoJanVinice <158711848+StoDomingoJanVinice@users.noreply.github.com>
2026-01-31 14:30:42 +0800

    Update bookViews.py

--- views/bookViews.py
+++ views/bookViews.py
@@ -1,1 +1,24 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../static/style.css">
+    <title>Book Catalog</title>
+</head>
+<body>
+    <h2>Library Catalog</h2>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Status</th>
+        </tr>
+        <tr>
+            <td>The Great Gatsby</td>
+            <td>F. Scott Fitzgerald</td>
+            <td>Available</td>
+        </tr>
+    </table>
+    <br>
+    <a href="../templates/index.html">Back to Home</a>
+</body>
+</html>
